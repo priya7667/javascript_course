d3cfe4a8426a30f55c7979c4a6a26a218da5e81e
Priyanjali Barriar <81919404+priya7667@users.noreply.github.com>
2023-10-17 06:11:46 +0000

    variables and datatypes

---
+++ .devcontainer/01_basics/01_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all code as newer version
+
+//alert(2+3)  //We are using nodejs not browser
+
+console.log(4
+    +3) //code readability should be high
+
+let name="Priya"
+let age=18
+let isLoggedIn=true
+
+//number=> 2 to power 53
+//bigint
+//string=>""
+//boolean=> true/false
+//null=>Standalone value
+//undefined=>variable declared but not assigned with a value
+console.log(typeof "Priya"); //string
+console.log(typeof null); //object
---
+++ .devcontainer/01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId=122456
+let accountEmail="priya@google.com"
+var accountPassword=12345
+
+// accountId=2  Not allowed
+
+/*
+
+Prefer not to use var
+*/
+accountEmail="ankit@34gj.com"
+accountPassword=123
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword]);
