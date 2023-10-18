232fc21e1ade21eaa9deb6b4715f7b355fc4a0fb
Priyanjali Barriar <81919404+priya7667@users.noreply.github.com>
2023-10-18 11:29:36 +0000

    Conversion operation

---
+++ .devcontainer/01_basics/04_conversion_operation.js
@@ -0,0 +1,34 @@
+let value=3
+let negValue=-value
+// console.log(negValue);
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1="Hello"
+let str2="Priyanjali"
+let str3=str1+str2
+console.log(str3);
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "3");
+
+//Postfix and Prefix
+
+let a1=20;
+let a2=++a1;
+let a3=a1++;
+console.log(a2);
+console.log(a3);
+
+
+//Comparing with null
+
+console.log(null>0)
+console.log(null==0);
+console.log(null<=0);
+console.log(null>=9);
+console.log("2">1)
+console.log("2"===2)
