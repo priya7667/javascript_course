6676a2ca862e364a7a5582d79f0417aaf2ea94c8
Priyanjali Barriar <81919404+priya7667@users.noreply.github.com>
2023-10-24 11:17:32 +0000

    Functions in js basics(part-2)

---
+++ 03_basics/02_functions.js
@@ -0,0 +1,16 @@
+function calculateCartPrize(num1,num2,...num3)
+{
+    return num3
+}
+console.log(calculateCartPrize(1,2,7,9,7));
+
+
+
+//Rest Parameters(It is used to represent infinite number of arguments as an array)
+function calculatePrize(...num1)
+{
+    return num1
+}
+console.log(calculatePrize(250,200,600,800))
+
+
