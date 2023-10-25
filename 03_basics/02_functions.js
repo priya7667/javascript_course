a16f5f21e9901a851ec1ef5346528c6008400016
Priyanjali Barriar <81919404+priya7667@users.noreply.github.com>
2023-10-25 06:14:13 +0000

    Functions in js(part-2) new

--- 03_basics/02_functions.js
+++ 03_basics/02_functions.js
@@ -12,5 +12,27 @@ function calculatePrize(...num1)
     return num1
 }
 console.log(calculatePrize(250,200,600,800))
+  
 
+//Functions with arrays
+
+let myNewArray=[200,300,400,500,600]
+function returnSecondValue(getArray)
+{
+     return getArray[1]
+}
+console.log(returnSecondValue(myNewArray));
+
+
+//Functions with objects
+let user=
+{
+    username:'Ananta',
+    price:1999
+}
+function returnValue(getObject)
+{
+    return getObject
+}
+console.log(returnValue(user));
 
