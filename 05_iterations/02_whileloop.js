89ce8f0163272f826e747ed4a9deb3d418483466
Priyanjali Barriar <81919404+priya7667@users.noreply.github.com>
2023-10-28 11:14:40 +0000

    While loop in js

---
+++ 05_iterations/02_whileloop.js
@@ -0,0 +1,15 @@
+let arr1=0
+while(arr1<10)
+{
+    console.log(arr1);
+    arr1+=2
+}
+
+
+let myArray = ['flash', "batman", "superman"]
+
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`Value is ${myArray[arr]}`);
+    arr = arr + 1
+}
