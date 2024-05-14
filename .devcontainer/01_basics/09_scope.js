b9aad6c7bff3abe20f6d053c6f76ead805b4d844
Priyanjali Barriar <81919404+priya7667@users.noreply.github.com>
2024-05-14 13:21:51 +0000

    more code on scopes

--- .devcontainer/01_basics/09_scope.js
+++ .devcontainer/01_basics/09_scope.js
@@ -1,4 +1,4 @@
-//global scope==> When a function is defined outside of any function or scope.
+//global scope==> When a variable is defined outside of any function or scope.
 
 let a="Aman";
 function f()
@@ -9,7 +9,7 @@ function f()
 f();
 console.log(a);
 
-//local scope==> when a function is defined within a function or block.
+//local scope==> when a variable is defined within a function or block.
 function f1()
 {
     let name="Sanskrit"
@@ -17,3 +17,16 @@ function f1()
 }
 f1();
 // console.log(name) name is not defined 
+
+let b=10;
+function f2()
+{
+    if(true)
+        {
+            let c=9;
+            console.log(c)
+        }
+        // console.log(c) error because of block scope
+}
+f2();
+console.log(b);
